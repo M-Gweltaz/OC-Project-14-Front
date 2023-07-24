0932e4ef14c5114bb80584fbaf08c56dd8fea3bf
Gweltaz <gweltazmary@gmail.com>
2023-07-24 22:34:35 +0200

    app routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import CreateEmployees from './pages/CreateEmployees';
+import EmployeeList from './pages/EmployeeList';
 
 function App() {
 	return (
 		<>
 			<Routes>
 				<Route path='/' element={<CreateEmployees />}></Route>
-				<Route></Route>
+				<Route path='/employee-list' element={<EmployeeList />}></Route>
 				<Route></Route>
 			</Routes>
 		</>
